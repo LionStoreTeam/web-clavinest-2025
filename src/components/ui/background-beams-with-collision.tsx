e021119a39db3c1f0d740e572309232f54643f38
LionStoreTeam <lionstoretechnologyteam@gmail.com>
2025-02-27 13:22:50 -0600

    Clavinet 27 Feb 2025

--- src/components/ui/background-beams-with-collision.tsx
+++ src/components/ui/background-beams-with-collision.tsx
@@ -70,7 +70,7 @@ export const BackgroundBeamsWithCollision = ({
     <div
       ref={parentRef}
       className={cn(
-        "bg-gradient-to-b from-white to-neutral-100 dark:from-neutral-950 dark:to-neutral-800 relative overflow-hidden",
+        "bg-transparent relative overflow-hidden",
         // h-screen if you want bigger
         className
       )}
@@ -87,7 +87,7 @@ export const BackgroundBeamsWithCollision = ({
       {children}
       <div
         ref={containerRef}
-        className="absolute bottom-0 bg-neutral-100 w-full inset-x-0 pointer-events-none"
+        className="absolute bottom-0 bg-transparent w-full inset-x-0 pointer-events-none"
         style={{
           boxShadow:
             "0 0 24px rgba(34, 42, 53, 0.06), 0 1px 1px rgba(0, 0, 0, 0.05), 0 0 0 1px rgba(34, 42, 53, 0.04), 0 0 4px rgba(34, 42, 53, 0.08), 0 16px 68px rgba(47, 48, 55, 0.05), 0 1px 0 rgba(255, 255, 255, 0.1) inset",
