761d8cd986aaa91d1786f73ac13eb5d669318d24
LionStoreTeam <lionstoretechnologyteam@gmail.com>
2025-03-05 12:53:41 -0600

    Clavinet 5 Mar 2025

--- src/app/_sections/Hero.tsx
+++ src/app/_sections/Hero.tsx
@@ -1,13 +1,12 @@
 import React from "react";
-import { BackgroundBeamsWithCollision } from "@/components/ui/background-beams-with-collision";
 import Image from 'next/image';
-import { Cover } from "@/components/ui/cover";
+import { BackgroundLines } from "@/components/ui/background-lines";
 
 
 
 export function Hero() {
     return (
-        <BackgroundBeamsWithCollision className="my-5 flex flex-col justify-center items-center text-center md:my-10">
+        <BackgroundLines className="my-5 flex flex-col justify-center items-center text-center">
             {/* <h2 className="mt-20 mb-10 text-2xl relative z-20 md:text-4xl lg:text-7xl font-bold text-center text-slate-600 dark:text-white font-sans tracking-tight">
                 Alivia tus dolores físico
                 <div className="my-5 mx-auto inline-block w-max [filter:drop-shadow(0px_1px_3px_rgba(27,_37,_80,_0.14))]">
@@ -22,9 +21,9 @@ export function Hero() {
                     Alivia tus dolores físicos
                 </p>
                 <br />
-                <Cover className="hover:text-[#e3d3c6]">Con Clavinest.</Cover>
+                <span className="hover:text-[#e3d3c6] transition-all ease-in-out duration-300">Con Clavinest.</span>
             </h1>
-            <Image src="/logo-b.png" alt="" width={500} height={500} className="float" />
-        </BackgroundBeamsWithCollision>
+            <Image src="/logo-w.png" alt='' width={350} height={350} className="float pt-10 xl:pt-32" />
+        </BackgroundLines>
     );
 }
